ede071a46dfa8764ab4d89e558660fd795cf885c
christian barillas <70426461+ChristianBarillas@users.noreply.github.com>
2023-02-19 21:21:24 -0600

    Agregando segundo ejercicio

--- base/config.js
+++ base/config.js
@@ -1,12 +1,12 @@
 
 
-const firebaseConfig = {
+// const firebaseConfig = {
    
-};
+// };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
 
-// Initialize Cloud Firestore and get a reference to the service
-const db = firebase.firestore();
+// // Initialize Cloud Firestore and get a reference to the service
+// const db = firebase.firestore();
--- index.js
+++ index.js
@@ -42,31 +42,70 @@ icono.style.display = ''
 
 // Calculador de notas
 
-const examen = document.querySelector('.examen');
-const tareas = document.querySelector('.tareas');
-const asistencia = document.querySelector('.asistencia');
-const investigacion = document.querySelector('.investigacion');
+//variables globales los addvenlistener no pueden retornar solo modificar variables globales
+var examen, tareas, asistencia, investigacion;
+var resultado;
 
-
-let nota1 = '';
-let nota2 = '';
-let noat3 = '';
-let nota4 = '';
+var r = [];
+function obtenerDatos( ) {
 
 
 
-examen.addEventListener('change', ()=>{
+examen = document.querySelector('.examen').value;
+tareas = document.querySelector('.tareas').value;
+asistencia = document.querySelector('.asistencia').value;
+investigacion = document.querySelector('.investigacion').value;
 
-    nota1 = examen.value;
-    
-});
-// tareas.addEventListener('chage', mostrarDatos);
-// asistencia.addEventListener('change', mostrarDatos);
-// investigacion.addEventListener('change', mostrarDatos);
-
-
-
-function mostrar(nota1){
-
-    console.log(nota1)
+  r = [examen,tareas,asistencia,investigacion];
+ 
+    return r
 }
+
+
+//se puede optimizar un poco mas para definir exactamente los input
+var input = document.getElementsByTagName("input");
+
+for (i=0; i<input.length; i++) {
+ input[i].addEventListener("change",  function(){
+  resultados = obtenerDatos();
+ 
+ });
+};
+
+const parrafo = document.querySelector('.estudiante-note')
+const carnet = document.querySelector('#carnet-estudiante')
+const nombreEstudiante = document.querySelector('#nombre-estudiante')
+// let fragment = document.createDocumentFragment();
+
+let misdatos = [];
+
+// se podria poner un boton con la funcion actualizar datos en vez de utilizar un set interval para optimizarlo
+//quizas se podria usar el request animation frame
+
+function actualizarDatos() {
+
+    
+    misdatos = obtenerDatos();
+    promedio = (parseFloat(misdatos[0])*20)/100 + (parseFloat(misdatos[1]) * 40 )/100 + (parseFloat(misdatos[2]) * 10)/100 + (parseFloat(misdatos[3])*30)/100;
+    
+    parrafo.innerHTML = `Estudiante: ${nombreEstudiante.value} <br> Carnet: ${carnet.value} <br> Nota promedio: ${promedio} `
+
+   
+
+  }
+  
+//   setInterval(actualizarDatos, 5000);
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
